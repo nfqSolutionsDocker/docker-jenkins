6c23d965300d440bb41f9476d7985175f078a608
raul_nunez <raul.nunez@nfq.es>
2017-06-26 11:07:35 +0200

    Added java split to jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -4,10 +4,14 @@
 
 echo Instalando java ...
 if [ ! -f /solutions/app/java/bin/java ]; then
-	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/raw/2.67-jdk8-python3.6.1/installations/aa"
-	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/raw/2.67-jdk8-python3.6.1/installations/ab"
+	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/blob/2.67-jdk8-python3.6.1/installations/aa"
+	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/blob/2.67-jdk8-python3.6.1/installations/ab"
+	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/blob/2.67-jdk8-python3.6.1/installations/ac"
+	wget -P /solutions/app/ "https://github.com/nfqSolutionsDocker/jenkins/blob/2.67-jdk8-python3.6.1/installations/ad"
 	cat /solutions/app/aa > /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz
 	cat /solutions/app/ab >> /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz
+	cat /solutions/app/ac >> /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz
+	cat /solutions/app/ad >> /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz
 	tar -xvzf /solutions/app/jdk-${JAVA_VERSION}-linux-x64.tar.gz -C /solutions/app/
 	chmod -R 777 $(ls -d /solutions/app/jdk*/)
 	ln -sf $(ls -d /solutions/app/jdk*/) /solutions/app/java
